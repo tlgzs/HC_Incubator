bab08a836e1dcd879f1b173e090eff97fb10d9de
xpwsgg <xpwsgg@163.com>
2018-11-12 18:05:02 +0800

    登录页面表单完成

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,10 +3,20 @@ namespace app\index\controller;
 
 use think\Controller;
 
+/**
+ * Class Index
+ * @package app\index\controller
+ */
 class Index extends Controller
 {
+    /**
+     * @return mixed
+     */
     public function index()
     {
         return $this->fetch();
+    }
+    public function dologin(){
+        \halt(\input());
     }
 }
