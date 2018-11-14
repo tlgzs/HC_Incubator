4402574e0ea58d4f6cc5105b55fb83a26d7a68c3
xpwsgg <xpwsgg@163.com>
2018-11-14 14:05:05 +0800

    管理后台账号密码完成;

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -4,6 +4,8 @@ namespace app\admin\controller;
 
 use app\admin\model\EnterpriseList;
 use app\admin\model\IncubateList;
+use app\index\model\User;
+use think\Db;
 
 /**
  * Class Index
@@ -19,28 +21,75 @@ class Index extends AdminBase
     public function index()
     {
         $key = \input('key', '');
+        $key2 = \input('key2', '');
+        $status = \input('status');
+        $year = \input('year');
         $enterpriseModel = new EnterpriseList();
         $list = $enterpriseModel->getEnterpriseListByCondition($key);
-//        \halt($list);
+
+        //这里不应该是孵化企业列表,而应该是报表列表!!!  下次回来改
+        $incubateModel = new IncubateList();
+        $incubate_list = $incubateModel->getIncubateListByCondition($year, $status, $key2);
+        //年份的数组
+        $min_year = Db::name('IncubateList')->min('create_time');
+        $min_year = \date('Y', $min_year);
+        $now_year = \date('Y');
+        for ($i = $min_year; $i <= $now_year; $i++) {
+            $years[] = \intval($i);
+        }
+        $this->assign('incubate_list', $incubate_list);
+        $this->assign('years', $years);
         $this->assign('list', $list);
         return $this->fetch();
     }
 
+    /**
+     * @return \think\response\Json
+     * @throws \Exception
+     * 添加孵化企业的操作
+     */
     public function selectIncubate()
     {
         $input = \input();
         $ids = $input['data'];
         foreach ($ids as $id) {
+            $incubate_info = EnterpriseList::get($id)->toArray();
             $sqldata[] = [
                 'incubate_id' => $id,
+                'incubate_name' => $incubate_info['enterprise_list_name'],
+            ];
+
+            //这两个可以合并一张表的,但是登录已经写好了就懒得改了
+            $sqldata2[] = [
+                'username' => $incubate_info['enterprise_list_name'],
+                'password' => '88888888',
             ];
         }
-        $model = new IncubateList();
-        $res = $model->saveAll($sqldata);
-        if ($res) {
+        $incubatemodel = new IncubateList();
+        $res = $incubatemodel->saveAll($sqldata);
+        $userModel = new User();
+        $res2 = $userModel->saveAll($sqldata2);
+        if ($res && $res2) {
             return \json(['code' => 1, 'msg' => 'Ok']);
         } else {
             return \json(['code' => 0, 'msg' => 'Fail']);
         }
+    }
+
+
+    /**
+     * @return mixed
+     * @throws \think\exception\DbException
+     * 企业的登录信息
+     */
+    public function accountInfo()
+    {
+        $userModel = new User();
+        $key = \input('key','');
+        $list = $userModel->where('group', 2)
+            ->whereLike('username','%'.$key.'%')
+            ->paginate(10);
+        $this->assign('list', $list);
+        return $this->fetch();
     }
 }
--- application/admin/model/IncubateList.php
+++ application/admin/model/IncubateList.php
@@ -11,7 +11,37 @@ namespace app\admin\model;
 
 use think\Model;
 
+/**
+ * Class IncubateList
+ * @package app\admin\model
+ */
 class IncubateList extends Model
 {
+    /**
+     * @var bool
+     */
     protected $autoWriteTimestamp = true;
+
+    public function incubateInfo()
+    {
+        return $this->hasOne('EnterpriseList', 'id', 'incubate_id');
+    }
+
+    public function getIncubateListByCondition($year, $status, $key)
+    {
+        $map = [];
+        if (empty($year)) {
+            $year = \date('Y');
+        }
+        if (!empty($status)) {
+            $map['status'] = $status;
+        }
+
+        $list = self::with('incubateInfo')
+            ->where($map)
+            ->whereLike('incubate_name', '%' . $key . '%')
+            ->whereBetweenTime('create_time', $year . '-' . '01' . '-' . '01', ($year + 1) . '-' . '01' . '-' . '01')
+            ->paginate(10);
+        return $list;
+    }
 }
